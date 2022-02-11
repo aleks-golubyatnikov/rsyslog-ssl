7bdcb1c63b7adb65ce7a109340eb9fa4d09d66ef
Golubyatnikov Aleksey <aleks.golubyatnikov@gmail.com>
2022-02-11 22:17:40 +0300

    --

--- generate-certificates.sh
+++ generate-certificates.sh
@@ -7,7 +7,7 @@ rm -rf $CERT_PATH/*.csr
 rm -rf $CERT_PATH/*.pem
 rm -rf $CERT_PATH/*.srl
 
-#CA
+#ca
 openssl genrsa -out $CERT_PATH/ca.key 2048
 openssl req -new -key $CERT_PATH/ca.key -subj "/CN=$CA_SERVER_LOCAL" -config $CERT_PATH/ca.cnf -out $CERT_PATH/ca.csr
 openssl x509 -req -days 365 -in $CERT_PATH/ca.csr -signkey $CERT_PATH/ca.key -extensions req_ext -extfile $CERT_PATH/ca.cnf -outform PEM -out $CERT_PATH/ca.crt
