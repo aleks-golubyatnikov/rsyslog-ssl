9593d3ce60f46130ccea11a719ad89b03fa7effe
Golubyatnikov Aleksey <aleks.golubyatnikov@gmail.com>
2022-02-11 20:16:46 +0300

    CN was changed - *.cnf

--- generate-certificates.sh
+++ generate-certificates.sh
@@ -8,7 +8,7 @@ openssl req -new -key $CERT_PATH/ca.key -subj "/CN=$CA_SERVER_LOCAL" -config $CE
 openssl x509 -req -days 365 -in $CERT_PATH/ca.csr -signkey $CERT_PATH/ca.key -extensions req_ext -extfile $CERT_PATH/ca.cnf -outform PEM -out $CERT_PATH/ca.crt
 
 #convert (if neaded)
-openssl x509 -inform DER -outform PEM -in $CERT_PATH/ca.crt -out $CERT_PATH/ca.crt.pem
+openssl x509 -outform PEM -in $CERT_PATH/ca.crt -out $CERT_PATH/ca.crt.pem
 
 #server
 openssl genrsa -out $CERT_PATH/$SERVER_LOCAL.key 2048
@@ -16,4 +16,4 @@ openssl req -new -key $CERT_PATH/$SERVER_LOCAL.key -subj "/CN=$SERVER_LOCAL" -co
 openssl x509 -req -days 365 -in $CERT_PATH/$SERVER_LOCAL.csr -CA $CERT_PATH/ca.crt.pem -CAkey $CERT_PATH/ca.key -CAcreateserial -extensions req_ext -extfile $CERT_PATH/server.cnf -out $CERT_PATH/$SERVER_LOCAL.crt
 
 #convert (if neaded)
-openssl x509 -inform DER -outform PEM -in $CERT_PATH/$SERVER_LOCAL.crt -out $CERT_PATH/$SERVER_LOCAL.crt.pem
+openssl x509 -outform PEM -in $CERT_PATH/$SERVER_LOCAL.crt -out $CERT_PATH/$SERVER_LOCAL.crt.pem
