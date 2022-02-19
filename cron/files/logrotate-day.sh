d34c672ceb038251f96a9b05773c39e439ea9136
Golubyatnikov Aleksey <aleks.golubyatnikov@gmail.com>
2022-02-19 21:23:15 +0300

    head restore

--- cron/files/logrotate-day.sh
+++ cron/files/logrotate-day.sh
@@ -5,12 +5,14 @@ DELETE_DAYS=$DEL_DAYS
 PATH_FILES=$APP_PATH_FILES
 PATH_LOG_FILE=$APP_PATH_LOG_FILE
 
+echo "$(date '+%Y-%m-%d %H:%M:%S') File rotation process started with options: [ROTATE_DAYS] => '$AGE_DAYS', [PATH_LOG_FILE] => '$APP_PATH_LOG_FILE' " >> $PATH_LOG_FILE;
 for d in $(find $PATH_FILES -mindepth 1 -maxdepth 1 -type d)
 do
+    echo $d
     for f in $(find $d -mindepth 1 -maxdepth 2 -type f -name "*.log" -mtime +"$((ROTATE_DAYS - 1))" | sort)
     do
         echo "$(date '+%Y-%m-%d %H:%M:%S') Compressing '$f' ... " >> $PATH_LOG_FILE;
-        if tar czfP "$f.$(date '+%Y_%m_%d_%H_%M').tar.gz" "$f"; then
+        if tar czf "$f.$(date '+%Y_%m_%d_%H_%M').tar.gz" "$f"; then
             echo "$(date '+%Y-%m-%d %H:%M:%S') Done" >> $PATH_LOG_FILE;
             rm -rf "$f";
         else
@@ -18,9 +20,12 @@ do
         fi
     done
 done
+echo "$(date '+%Y-%m-%d %H:%M:%S') File rotation process is finished" >> $PATH_LOG_FILE;
 
+echo "$(date '+%Y-%m-%d %H:%M:%S') File deletion process started with options: [DELETE_DAYS] => '$DEL_DAYS', [PATH_LOG_FILE] => '$APP_PATH_LOG_FILE' " >> $PATH_LOG_FILE;
 for d in $(find $PATH_FILES -mindepth 1 -maxdepth 1 -type d)
 do
+    echo d$
     for f in $(find $d -mindepth 1 -maxdepth 2 -type f -name "*.tar.gz" -mtime +"$((DELETE_DAYS - 1))" | sort)
     do
         echo "$(date '+%Y-%m-%d %H:%M:%S') Removing '$f' ... " >> $PATH_LOG_FILE;
@@ -31,3 +36,4 @@ do
         fi
     done
 done
+echo "$(date '+%Y-%m-%d %H:%M:%S') File deletion process is finished" >> $PATH_LOG_FILE;
