34be1cad002ca7b939d0df15f29d8fc0a48c75d2
Golubyatnikov Aleksey <aleks.golubyatnikov@gmail.com>
2022-02-20 12:54:00 +0300

    prod, dev ENV were added

--- rsyslog/start.sh
+++ rsyslog/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed -i "s/%SERVER_CA_FILE%/$CA_PATH/" /etc/rsyslog.conf
-sed -i "s/%SERVER_CERT_PATH%/$CERT_PATH/" /etc/rsyslog.conf
-sed -i "s/%SERVER_KEY_PATH%/$KEY_PATH/" /etc/rsyslog.conf
-sed -i "s/%SERVER_MAX_TCP_SESSIONS%/$MAX_TCP_SESSIONS/" /etc/rsyslog.conf
-sed -i "s/%SERVER_TLS_PORT%/$TLS_PORT/" /etc/rsyslog.conf
-sed -i "s/%SERVER_TEMPLATE_PATH%/$TEMPLATE_PATH/" /etc/rsyslog.conf
+sed -i "s|%SERVER_CA_FILE%|$CA_PATH|g" /etc/rsyslog.conf
+sed -i "s|%SERVER_CERT_PATH%|$CERT_PATH|g" /etc/rsyslog.conf
+sed -i "s|%SERVER_KEY_PATH%|$KEY_PATH|g" /etc/rsyslog.conf
+sed -i "s|%SERVER_MAX_TCP_SESSIONS%|$MAX_TCP_SESSIONS|g" /etc/rsyslog.conf
+sed -i "s|%SERVER_TLS_PORT%|$TLS_PORT|g" /etc/rsyslog.conf
+sed -i "s|%SERVER_TEMPLATE_PATH%|$TEMPLATE_PATH|g" /etc/rsyslog.conf
 
 rsyslogd -n -f /etc/rsyslog.conf
