2b35b0bcee6431941765a02b49f266a8c6c593b0
Golubyatnikov Aleksey <aleks.golubyatnikov@gmail.com>
2022-02-20 11:09:03 +0300

    ENV - vars

--- generate-config.sh
+++ generate-config.sh
@@ -2,7 +2,7 @@
 
 export $(cat .env | egrep -v "(^#.*|^$)" | xargs)
 
-sed -i "s/%%ca.pem%%/ca.crt.pem/" $CONF_PATH/rsyslog.conf
+sed -i "s/%%ca.pem%%/$CA_CRT_NAME/" $CONF_PATH/rsyslog.conf
 sed -i "s/%%server-cert.pem%%/$SERVER_LOCAL.crt.pem/" $CONF_PATH/rsyslog.conf
 sed -i "s/%%server-key.pem%%/$SERVER_LOCAL.key.pem/" $CONF_PATH/rsyslog.conf
 sed -i "s/%%_TLS_PORT_%%/$TLS_PORT_CONTAINER/" $CONF_PATH/rsyslog.conf
